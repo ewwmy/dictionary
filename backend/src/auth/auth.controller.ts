78d74e576a1527487c067af2046d6290cd0fb278
Andrew Akimov <akimov.it.dev@gmail.com>
2025-07-15 20:25:58 +0300

    Improved rate limiting

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -9,17 +9,40 @@ import {
 } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { AuthService } from './auth.service'
+import { Throttle } from '@nestjs/throttler'
+import { ConfigService } from '@nestjs/config'
+import {
+  THROTTLE_LOGIN_LIMIT,
+  THROTTLE_LOGIN_TTL,
+  THROTTLE_REGISTER_LIMIT,
+  THROTTLE_REGISTER_TTL,
+} from 'src/throttler/throttler.const'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {}
 
   @Post('register')
+  @Throttle({
+    default: {
+      ttl: THROTTLE_REGISTER_TTL,
+      limit: THROTTLE_REGISTER_LIMIT,
+    },
+  })
   register(@Body() dto: { name: string; email: string; password: string }) {
     return this.authService.register(dto)
   }
 
   @Post('login')
+  @Throttle({
+    default: {
+      ttl: THROTTLE_LOGIN_TTL,
+      limit: THROTTLE_LOGIN_LIMIT,
+    },
+  })
   async login(@Body() dto: { email: string; password: string }) {
     const user = await this.authService.validateUser(dto.email, dto.password)
     if (!user) throw new UnauthorizedException()
